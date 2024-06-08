a11b1056b107df585b798e292cbdfaf3e02220f4
rodrigoreact <ttdevelopreact@gmail.com>
2024-06-08 16:09:03 -0400

    Se modifica setting

--- todoproy/settings.py
+++ todoproy/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-^ag&#%(spw(%fou+u&llctljv!w6odvp@bm#0k6tfpb-b7c=(=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['todo-3730.onrender.com']
+CSRF_TRUSTED_ORIGINS=['https://todo-3730.onrender.com']
 
 # Application definition
 
